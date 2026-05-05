0c6fe6063fdabe1b8b2b3cf1a47bc20d0a073ba9
tamori <k.m.biz169@gmail.com>
2026-05-05 14:36:17 +0900

    完成バージョン保存

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
